f47890390501a7ca0ff12f9544166680d3935e8a
NitaGIT1 <nitashingade18@gmail.com>
2026-02-09 12:35:08 +0530

    assignment 1

---
+++ src/Variables/AssignmentQ3.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class AssignmentQ3 {
+
+	public static void main(String[] args) {
+		String a = "Hello";
+		String b = "World";
+		System.out.println(a+" "+b);
+			
+
+	}
+
+}
