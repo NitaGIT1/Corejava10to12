556399723b996f41066e8315a3c1c9dad53065df
NitaGIT1 <nitashingade18@gmail.com>
2026-02-09 12:15:18 +0530

    Initial commit

---
+++ src/Variables/DifferentDatatypes.java
@@ -0,0 +1,24 @@
+package Variables;
+
+public class DifferentDatatypes {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		double b = 10.10;  
+		char c ='A';
+		boolean flag = true;
+		String name  = "Nita";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(flag);
+		System.out.println(name);
+		
+		
+		
+
+
+	}
+
+}
